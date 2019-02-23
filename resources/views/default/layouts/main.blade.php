8b71938e2c2fad33270764169b8016b395bfbcc3
guaosi <guaosi>
2019-02-23 21:37:26 +0800

    修改部分显示

--- resources/views/default/layouts/main.blade.php
+++ resources/views/default/layouts/main.blade.php
@@ -85,7 +85,9 @@
         <div class="row text-center">
             <div class="col-lg-12">
                 <p class="text-muted">
-                    <span id="busuanzi_container_site_pv">本站总访问量<span id="busuanzi_value_site_pv"></span>次</span>
+                    <span id="busuanzi_container_site_uv">
+  本站访客数<span id="busuanzi_value_site_uv"></span>人次
+</span>
                 </p>
             </div>
         </div>
