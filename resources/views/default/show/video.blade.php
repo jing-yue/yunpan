d99fbff6a6374ff679642b5de1adc4b0cfc0941f
guaosi <guaosi>
2019-03-14 09:19:02 +0800

    修复地址

--- resources/views/default/show/video.blade.php
+++ resources/views/default/show/video.blade.php
@@ -25,10 +25,13 @@
                 <div id="video-player">
                     <video crossorigin playsinline controls poster="{!! $file['thumb'] !!}" id="player">
                         <source src="{!! $file['download'] !!}" type="video/mp4">
+                        @if(!empty($file['subtitle']))
+                            <track kind="captions" label="字幕" src="{!! $file['subtitle'] !!}" srclang="中文" default />
+                        @endif
                     </video>
                 </div>
                 <br>
-                <p class="text-danger">如无法播放或格式不受支持，推荐使用 <a href="https://pan.lanzou.com/b112173" target="_blank">potplayer</a>
+                <p class="text-danger">如无法播放或格式不受支持，推荐使用 <a href="https://cloud.guaosi.com/home/Video/%E6%92%AD%E6%94%BE%E5%99%A8/windows/PotPlayer" target="_blank">Potplayer(windows)</a> 或 <a href="https://cloud.guaosi.com/home/Video/%E6%92%AD%E6%94%BE%E5%99%A8/mac" target="_blank">Movist(Mac)</a>
                     播放器在线播放
                 </p>
                 <label class="control-label">下载链接</label>
@@ -47,3 +50,4 @@
         </div>
     </div>
 @stop
+
